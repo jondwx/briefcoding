4226d2494398326a9710d950704eb0f128bc645e
jondwx <jondwx@gmail.com>
2017-11-13 23:16:54 +0700

    init

---
+++ index.php
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<!DOCTYPE html>
+<html b:css='false'>
+  &lt;head&gt;
+    <title><data:blog.title/></title>
+    <b:skin></b:skin>
+  &lt;/head&gt;&lt;!--<head/>--&gt;
+  <body>
+    <b:section id='content'>
+      <?php include('blog.php') ?>
+    </b:section>
+    &lt;!--</body>--&gt;&lt;/body&gt;
+</html>
