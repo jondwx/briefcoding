498014cd15286e92d007cdb8aafee222a0e6d61d
jondwx <jondwx@gmail.com>
2017-11-15 05:38:41 +0700

    convert text to HTML

--- script.php
+++ script.php
@@ -1,10 +1,27 @@
-<b:if cond='data:blog.pageType == &quot;index&quot;'>
 <script>
+  //<![CDATA[
   $(function() {
-    var text = $("#header-content .d-none").html();
-    var texts = text.split(" ~ ");
-    $("#header-content .title").text(texts[0]);
-    $("#header-content .lead").text(texts[1]);
+
+    function convertTextToHTML(text) {
+      var result = text;
+      var matches = text.match(/\*(.*?)\*/g);
+      for (var i = 0;matches != null && i < matches.length;i++) {
+        var match = matches[i];
+        var baseText = match.replace(/\*/g, "");
+        var changeTo = {match: match, to: "<b>"+baseText+"</b>"};
+        result = result.replace(changeTo.match, changeTo.to);
+      }
+      return result;
+    }
+
+    var theE = $("#header-content .d-none");
+    if (theE.length) {
+      var text = theE.html();
+      var texts = text.split(" ~ ");
+      $("#header-content .title").text(texts[0]);
+      $("#header-content .lead").html(convertTextToHTML(texts[1]));
+    }
+
   });
+  //]]>
 </script>
-</b:if>
