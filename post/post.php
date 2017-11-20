975997372642d3a400cb550d56c3519b3af4602e
jondwx <jondwx@gmail.com>
2017-11-20 13:35:19 +0700

    update

--- post/post.php
+++ post/post.php
@@ -5,6 +5,7 @@ This post explains how I build Tic Tac Toe Game In 3 simple steps<!--more-->
 
 <style>
 table.tic-tac-toe-board {
+  margin: 0 auto;
   border: 4px solid black;
   border-spacing: 0;
 }
@@ -17,28 +18,36 @@ table.tic-tac-toe-board td {
 
 <h2>#1 The Board</h2>
 
-<table class="tic-tac-toe-board">
-  <tr>
-    <td></td>
-    <td></td>
-    <td></td>
-  </tr>
-  <tr>
-    <td></td>
-    <td></td>
-    <td></td>
-  </tr>
-  <tr>
-    <td></td>
-    <td></td>
-    <td></td>
-  </tr>
-</table>
+<div>
+  <table class="tic-tac-toe-board">
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </table>
+  <div style="text-align: center;">
+    <em>The Board</em>
+  </div>
+</div>
 
 <p>
 Start with creating the board.
 In this case the game needs a board that has 3x3 grid,
 something like the figure above.
-I decided to use HTML element to create the base of the game.
+I decided to use HTML element to create base canvas for the game.
 But, what element can create something like that?
+I think there are several ways to achieve that look, I can use
+nested <code>div</code>, but this time I will go with <code>table</code>
+element, because I guess that is the easiest.
 </p>
